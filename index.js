24626e8689b04676592c57da3ff3f81c54f135dd
Allan <mugabialan@gmail.com>
2020-10-17 19:41:46 +0300

    Added a new enpoint /api/courses returning a response to the user

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
     res.send('You have reached the server');
 } );
 
+app.get('/api/courses', (req,res) => {
+    res.send([1, 2, 3]);
+});
+
 app.listen(3000, () => console.log('Listening on port 3000...'));
